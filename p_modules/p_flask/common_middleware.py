67f9ba0ad97687e02410f6a491391d8b6d62dc7d
liuyingpeng <heylester@outlook.com>
2020-10-14 17:40:53 +0800

    Flask自定义过滤器模块

--- p_modules/p_flask/common_middleware.py
+++ p_modules/p_flask/common_middleware.py
@@ -3,6 +3,10 @@
 #  author: Liu Yingpeng
 
 class MyMiddleWare(object):
+    """
+    app = Flask(__name__)
+    app.wsgi_app = MyMiddleWare(app.wsgi_app)
+    """
 
     def __init__(self, wsgi_app):
         self.wsgi_app = wsgi_app
